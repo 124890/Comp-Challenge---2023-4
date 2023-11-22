07f22dea2de4e12ff749787ad646d4b8d9e2d8f1
124890 <douglaspenning@outlook.com>
2023-11-22 11:06:48 +0000

    Initial v.1
    Initial version, starting pseudo and template

---
+++ Code.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#import data from the files
+dataset = pd.read_csv('Concrete_database.csv')
+
+#Data Preprocessing
+
+#how do i push this to github
